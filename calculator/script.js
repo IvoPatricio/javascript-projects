b47307e1d1abb9cb757caa7b3df1c9848a8fad25
IvoPatricio <snoukefm@gmail.com>
2024-09-21 18:16:10 +0100

    added_calculations_to_result

--- calculator/script.js
+++ calculator/script.js
@@ -6,7 +6,7 @@ function handleButtonClick(event) {
 
     if (button.classList.contains('equal'))
     {
-        //CALCULATE HERE
+        document.getElementById('resultDisplay').textContent = calculateResult(values);
         values = [];
     }
     else if (button.classList.contains('clear'))
@@ -21,6 +21,52 @@ function handleButtonClick(event) {
     }
 }
 
+function calculateResult(values)
+{
+    const numbers = values.join('');
+    let result = 0;
+    let currentNumber = '';
+    let operator = '+';
+    for (let i = 0; i < numbers.length; i++)
+    {
+        const char = numbers[i];
+        if (char >= '0' && char <= '9') 
+        {
+            currentNumber += char;
+        }
+        else
+        {
+            if (currentNumber != null)
+            {
+                const num = parseFloat(currentNumber);
+                if (operator == '+')
+                    result += num;
+                else if (operator == '-')
+                    result -= num;
+                else if (operator == '/')
+                    result /= num;
+                else if (operator == '*')
+                    result *= num;
+                currentNumber = '';
+            }
+            operator = char;
+        }
+    }
+    if (currentNumber != null)
+    {
+        const num = parseFloat(currentNumber);
+        if (operator == '+')
+            result += num;
+        else if (operator == '-')
+            result -= num;
+        else if (operator == '/')
+            result /= num;
+        else if (operator == '*')
+            result *= num;
+        currentNumber = '';
+    }
+    return result;
+}
 //adding events to each buttong to check when the buttons are clicked
 const buttons = document.querySelectorAll('.digit, .operator, .equal, .clear');
 buttons.forEach(button => 
